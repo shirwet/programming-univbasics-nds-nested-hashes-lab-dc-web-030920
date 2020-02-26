4e98468265dce7d71d41863dbe3c44eae38fdeb3
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-26 21:24:33 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,8 +25,8 @@ def literal_aoh
   new_array = [ :DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
 :JOELLE_VD = { name: "Joelle van Dyne", occupation: "Radio Personality" },
 :PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
-KATE_G = { name:  "Kate Gompert", occupation: "None" },
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
+:KATE_G = { name:  "Kate Gompert", occupation: "None" },
+:BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end
 
 def aoh_lookup(aoh, row, key)
